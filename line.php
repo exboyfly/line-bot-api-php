d87f25b8b2119b241e5cc1c590baf3b65037b06a
exboyfly <50616941+exboyfly@users.noreply.github.com>
2020-04-14 14:52:58 +0700

    Update line.php

--- line.php
+++ line.php
@@ -38,7 +38,7 @@ if ( sizeof($request_array['events']) > 0 )
 			$sell = $response->response->price->gold->sell;
 			
 			$reply_message = "ราคาทองคำวันที่" .$date."เวลา " .$updateTime." ราคา ".$buy." บาท"." ราคาขาย ".$sell." บาท";
-		}else (){
+		}else {
 			$reply_message = "ขอโทษด้วย ฉันไม่เข้าใจสิ่งนี้";
 		}
    }
